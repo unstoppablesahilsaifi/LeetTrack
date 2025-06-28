b5930d8195846c1d49b123732dac0743680d7379
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-06-28 12:48:05 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/leettrack/LeettrackApplication.java
@@ -0,0 +1,21 @@
+package com.leettrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication(exclude = {
+		org.springframework.ai.vectorstore.redis.autoconfigure.RedisVectorStoreAutoConfiguration.class
+})
+public class LeettrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeettrackApplication.class, args);
+	}
+	// Log message after full startup
+	@EventListener(ApplicationReadyEvent.class)
+	public void logStartup() {
+		System.out.println("✅ Application is running successfully");
+	}
+}
