83a2782845320fc95918e768ee9770d87d4beeff
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-06-28 15:40:53 +0530

    swagger added

--- src/main/java/com/leettrack/config/SecurityConfig.java
+++ src/main/java/com/leettrack/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.leettrack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    // Security filter chain config
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/h2-console/**",     // optional
+                                "/api/v1/auth/**"     // your auth APIs
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
